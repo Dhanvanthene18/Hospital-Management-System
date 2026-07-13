462cbc79a5be204c6156679e501a1163e63181fe
S.Dhanvanthene <dhanasaravanan150603@gmail.com>
2026-07-13 11:32:17 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# -----------------------------
+# Database Configuration
+# -----------------------------
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:DG@1847@localhost/hospital_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# -----------------------------
+# Home Route
+# -----------------------------
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# -----------------------------
+# Run Application
+# -----------------------------
 if __name__ == "__main__":
     app.run(debug=True)
